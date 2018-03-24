323d73113d543a3e8bf080b1a714400311435161
soudazhou <soudazhou@gmail.com>
2018-03-24 13:49:10 +0000

    JiuZhang[BinarySearch] - solution for //http://www.lintcode.com/en/problem/search-for-a-range/

--- src/Jiuzhang/BinarySearch.java
+++ src/Jiuzhang/BinarySearch.java
@@ -96,4 +96,62 @@ public class BinarySearch {
 
         return -1;
     }
+
+    //http://www.lintcode.com/en/problem/search-for-a-range/
+    public int[] searchRange(int[] A, int target) {
+        if (A == null || A.length == 0) {
+            return new int[]{-1, -1};
+        }
+
+        int start = 0;
+        int end = A.length - 1;
+        int mid = 0;
+        int[] ans = new int[2];
+
+        //search for left bound
+        while (start + 1 < end) {
+            mid = start + (end - start) / 2;
+            if (A[mid] == target) {
+                end = mid;
+            } else if (A[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (A[start] == target) {
+            ans[0] = start;
+        } else if(A[end] == target) {
+            ans[0] = end;
+        } else {
+            ans[0] = ans[1] = -1;
+            return ans;
+        }
+
+     //search for right bound
+        start = 0;
+        end = A.length - 1;
+        //search for left bound
+        while (start + 1 < end) {
+            mid = start + (end - start) / 2;
+            if (A[mid] == target) {
+                end = mid;
+            } else if (A[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (A[end] == target) {
+            ans[1] = end;
+        } else if(A[start] == target) {
+            ans[0] = start;
+        } else {
+            ans[0] = ans[1] = -1;
+            return ans;
+        }
+        return ans;
+    }
 }
